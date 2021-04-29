152376dd411ac0245c6027e9a2b7875267eafa4f
Mohammed <mohammedlotf@gmail.com>
2021-04-30 00:34:04 +0300

    Register button is deleted

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,9 +29,9 @@
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
-                        @if (Route::has('register'))
+                        {{-- @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
+                        @endif --}}
                     @endauth
                 </div>
             @endif
