368a077c43dc8785a2fb79acc170b92ba8c7c0fa
khalil <khalilali1017@gmail.com>
2021-04-29 23:57:21 +0300

    add migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
@@ -14,10 +14,20 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->increments('id');
+            $table->string('user_name_third');//الاسم الثلاثي
+            $table->string('user_surname');//اللقب
+            $table->string('user_type');
+            $table->string('sex');
+            $table->date('birthdate');
+            $table->string('birthplace');//المحافظه
+            $table->string('town');//المديرية
+            $table->string('village');//العزلة
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone_number');
+            $table->string('identity_type');//نوع الهوية
+            $table->string('identity_number');//نوع الهوية
+            $table->binary('user_image');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
